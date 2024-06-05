04777623f1743fdf1c64264a4de5a675086d1f6f
Marcos <marcos.eng.software@gmail.com>
2024-06-05 04:37:35 -0300

    0

--- Main.java
+++ Main.java
@@ -1,25 +1,29 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		
-		double x = 10.35784;
 		
-        System.out.println(x);
-        System.out.printf("%.2f%n", x);
-        System.out.printf("%.4f%n", x);
-        
-        Locale.setDefault(Locale.US);
-        System.out.printf("%.4f%n", x);
-        System.out.println("RESULTADO = " + x + " METROS");
-        System.out.printf("RESULTADO =  %.2f metros%n", x);
-        
-        String nome = "Maria";
-        int idade = 7;
-        double renda = 5.0;
-        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+		  Locale.setDefault(Locale.US);
+	      Scanner sc = new Scanner(System.in);
+	      
+	      String x;
+	      int y;
+	      double z;
+	      
+	      x = sc.next();
+	      y = sc.nextInt();
+	      z = sc.nextDouble();
+	      
+	      System.out.println("Dados digitados");
+	      System.out.println(x);
+	      System.out.println(y);
+	      System.out.println(z);
+	      
+	      sc.close();
         
         
 	}
