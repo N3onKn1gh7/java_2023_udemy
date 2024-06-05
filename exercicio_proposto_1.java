3406d52050a0ec85e1fe9a8ba0c8adaed77da4de
Marcos <marcos.eng.software@gmail.com>
2024-06-05 20:50:32 -0300

    Exercicio para iniciantes oarte 1

---
+++ exercicio_proposto_1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercicio_proposto_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int valor1 = sc.nextInt();
+		int valor2 = sc.nextInt();
+		
+		int soma = valor1 + valor2;
+		
+		System.out.println("A Soma de " + valor1 +  " e " + valor2 + " é: " + soma);
+		
+		sc.close();
+		
+		
+		
+		
+		
+				
+	
+
+	}
+
+}
