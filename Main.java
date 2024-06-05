7bb6ccc6c139096c3bdc4d27b7b7c72f46ef498b
Marcos <marcos.eng.software@gmail.com>
2024-06-05 16:16:29 -0300

    0

--- Main.java
+++ Main.java
@@ -4,28 +4,27 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		
+		int x;
+		String s1, s2, s3;
 		
+		x = sc.nextInt();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
 		
-		  Locale.setDefault(Locale.US);
-	      Scanner sc = new Scanner(System.in);
-	      
-	      String x;
-	      int y;
-	      double z;
-	      
-	      x = sc.next();
-	      y = sc.nextInt();
-	      z = sc.nextDouble();
-	      
-	      System.out.println("Dados digitados");
-	      System.out.println(x);
-	      System.out.println(y);
-	      System.out.println(z);
-	      
-	      sc.close();
-        
-        
+		System.out.println("DADOS DIGITADOS: ");
+		
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+
+		sc.close();
+
 	}
 
 }
