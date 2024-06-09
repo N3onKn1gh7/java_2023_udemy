3084b0074b42c0f5689c014aa42a641284aa5926
Marcos <marcos.eng.software@gmail.com>
2024-06-09 13:10:20 -0300

    exercicios para iniciantes parte 2

--- exercicio_proposto_6.java
+++ exercicio_proposto_6.java
@@ -9,25 +9,25 @@ public class exercicio_proposto_6 {
 		Scanner sc = new Scanner(System.in);
 
 		double A, B, C, tri, raio, trap, quad, ret, pi;
-		
+
 		pi = 3.14159;
 
 		A = sc.nextDouble();
 		B = sc.nextDouble();
 		C = sc.nextDouble();
-		
-		tri =  (A * C) / 2.0;
+
+		tri = (A * C) / 2.0;
 		raio = (pi * Math.pow(C, 2));
 		trap = ((A + B) * C) / 2.0;
 		quad = Math.pow(B, 2);
-		ret  = A * B; 
-		
+		ret = A * B;
+
 		System.out.printf("TRIANGULO: %.3f%n", tri);
 		System.out.printf("CIRCULO: %.3f%n", raio);
 		System.out.printf("TRAPEZIO: %.3f%n", trap);
 		System.out.printf("QUADRADO: %.3f%n", quad);
 		System.out.printf("RETANGULO: %.3f%n", ret);
-
+		sc.close();
 	}
 
 }
