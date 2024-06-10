b4251b63682d7872df04bf0eda70b3da750c5604
Marcos <marcos.eng.software@gmail.com>
2024-06-10 11:16:42 -0300

    0

--- Main.java
+++ Main.java
@@ -4,30 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
 		Scanner sc = new Scanner(System.in);
-		int hora;
-		
-		System.out.println("Quantas horas?");
-		hora = sc.nextInt();
 
-		
-		if (hora < 12) {
-		System.out.println("bom dia");
+		int x = sc.nextInt(); /* recebe um valor do usuario na variavel x*/
+
+		int soma = 0; 
+
+		while (x != 0) {      /*Enquanto x for diferente de 0 as instruçoes do bloco vao se repetir*/
+
+			soma = soma + x;  /* A variavel soma vai acumular o valor de x*/
+			x = sc.nextInt(); /* A variavel soma vai receber o valor de x enquanto x for diferente de 0*/
 		}
-		else if (hora < 18){
-				System.out.println("boa tarde");
-			}	
-		else {
-				System.out.println("Boa noite");
-			}
-		}
-		
-		
+
+		System.out.println(soma); /*Quando x for igual a zero, a estrutura repetitiva para e o comando de impressao na tela é realizado mostrando a soma dos falores acumulados*/
+
 		sc.close();
-		
 
-		
 	}
 
 }
